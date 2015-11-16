125c555ca67bca693f884d901159ca9849d4c623
Spyes <lnitzberg@gmail.com>
2015-11-15 22:11:30 -0800

    hash

--- hash/hash-chaining.c
+++ hash/hash-chaining.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define HASH_SIZE 59
 
 struct node {
     int val;
+    char *str;
     struct node *next;
 };
 typedef struct node item;
 
-item *create_node(int val)
+item *create_node(int val, char *str)
 {
     item *temp = (item*)malloc(sizeof(item));
     temp->val = val;
+    temp->str = str;
     temp->next = NULL;
     return temp;
 }
@@ -22,13 +25,26 @@ int hash_function(int val)
     return val % HASH_SIZE;
 }
 
+int hash_string_function(char *str)
+{
+    int hashVal = 0;
+    int ch, i;
+    for (i = 0, ch = str[i]; ch != '\0'; i++, ch = str[i]) {
+	hashVal = (hashVal * 27 + (ch - '0')) % HASH_SIZE;
+    }
+    return hashVal;
+}
+
 void insert(item **hashTable, item **node)
 {
-    int val = (*node)->val;
-    int key = hash_function(val);
-    //    item *node = create_node(val);
-    if (hashTable[val] == NULL)
-        hashTable[val] = (*node);
+    int key;
+    if ((*node)->val && (*node)->val > -1)
+	key = hash_function((*node)->val);
+    else if ((*node)->str)
+	key = hash_string_function((*node)->str);
+
+    if (hashTable[key] == NULL)
+        hashTable[key] = (*node);
     else {
         item *curr = hashTable[key];
         while(curr->next != NULL)
@@ -37,16 +53,27 @@ void insert(item **hashTable, item **node)
     }
 }
 
-item *find(item **hashTable, int val)
+item *find(item **hashTable, int val, char *str)
 {
-    int key = hash_function(val);
+    int key;
+    if (val > -1)
+	key = hash_function(val);
+    else if (str)
+	key = hash_string_function(str);
     item *curr = hashTable[key];
     while (curr) {
-	if (curr->val == val)
+	if ((val > -1 && curr->val == val) ||
+	    (str && (strcmp(str, curr->str) == 0)))
 	    return curr;
 	curr = curr->next;
     }
     return NULL;
+}
+
+void print(item *node)
+{
+    if (node)
+	node->str ? printf("%s\n", node->str) : printf("%i\n", node->val);
 }
 
 int main(int argc, char **argv)
@@ -56,17 +83,18 @@ int main(int argc, char **argv)
     for (j = 0; j < HASH_SIZE; j++)
 	hashTable[j] = NULL;
 
-    item *n = create_node(10);
+    item *n = create_node(-1, "test");
     insert(hashTable, &n);
-    n = create_node(69);
+    n = create_node(10, NULL);
     insert(hashTable, &n);
 
-    item *f = find(hashTable, 69);
-    printf("%i\n", f->val);
-    f = find(hashTable, 10);
-    printf("%i\n", f->val);
+    item *f = find(hashTable, -1, "test");
+    print(f);
+    f = find(hashTable, 10, NULL);
+    print(f);
 
-    free(f);
-    free(n);
+    for (j = 0; j < HASH_SIZE; j++)
+	if (hashTable[j]) free(hashTable[j]);
+
     return 0;
 }
